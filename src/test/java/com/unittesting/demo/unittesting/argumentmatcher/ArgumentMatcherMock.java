44a09a9d6d9106269bae978dc471ae264700834a
Sachin <sachinkmr375@gmail.com>
2021-07-04 22:11:20 +0530

    verify usage.

---
+++ src/test/java/com/unittesting/demo/unittesting/argumentmatcher/ArgumentMatcherMock.java
@@ -0,0 +1,38 @@
+package com.unittesting.demo.unittesting.argumentmatcher;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ArgumentMatcherMock {
+
+    @Mock
+    private List listMock;
+
+    @Test
+    public void ShouldReturnString() {
+        when(listMock.get(anyInt())).thenReturn("Sample String");
+        assertEquals("Sample String", listMock.get(0));
+        assertEquals("Sample String", listMock.get(2));
+    }
+
+    @Test
+    public void testForVerifyTheCall() {
+        listMock.get(0);
+        listMock.get(1);
+
+        verify(listMock).get(0); //Has it called or not.
+        verify(listMock, times(1)).get(0);
+        verify(listMock, times(2)).get(anyInt());
+    }
+}
